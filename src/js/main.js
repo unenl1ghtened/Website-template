a51502408313c3f6adcfe08b1c2b359ca892f6df
Margarita <margarita0work@gmail.com>
2025-04-09 18:01:44 +0300

    Finished contact section naming with _1 and renamed image

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,10 @@ import { useTheme } from './components/theme.js';
 import { useTestimonialsSlider } from './components/home/testi-slider.js';
 import { useTeamSlider } from './components/home/team-slider.js';
 import { usePhone } from './components/home/phone.js';
-import { useInputValidation } from './components/home/input-validation.js';
 
 useBurger();
 useHeader();
 useTestimonialsSlider();
 useTeamSlider();
 usePhone();
-useInputValidation();
 useTheme();
